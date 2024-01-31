968a9d8774a4f9a02781ad9cfe67d363b38cb3fa
Amrit Singh <amrit.singh@accesshq.com>
2024-02-01 08:55:20 +1030

    Initial Commit

---
+++ Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
+
+        public PagingInfo PagingInfo { get; set; } = new();
+
+        public string? CurrentCategory { get; set; }
+
+
+    }
+}
